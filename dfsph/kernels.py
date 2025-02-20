c3daca28c026a044a2f73e95027e1929ab45651d
davidAlgis <david.algis@tutamail.com>
2025-02-20 15:15:10 +0100

    ✨ Add another implementation of kernel

--- dfsph/kernels.py
+++ dfsph/kernels.py
@@ -66,9 +66,49 @@ def grad_kernel_cubic_spline(xI, xJ, h):
     return (dWdr / r_length) * r
 
 
+# another implementation of cubic spline kernel that comes from https://github.com/MmmmHeee/SPH-2D-Taichi/blob/master/src/smooth_kernel.py
+__CUBIC_KERNEL_FACTOR = 10 / (7 * np.pi)
+
+
+def cubic_kernel(xI, xJ, h):
+    # Value of cubic spline smoothing kernel
+    r = np.array(xI) - np.array(xJ)
+    r_len = np.linalg.norm(r)
+
+    half_h = h / 2
+    k = __CUBIC_KERNEL_FACTOR / half_h**2
+    q = r_len / half_h
+
+    res = 0.0
+    if q <= 1.0:
+        q2 = q**2
+        res = k * (1 - 1.5 * q2 + 0.75 * q * q2)
+    elif q < 2.0:
+        res = k * 0.25 * (2 - q)**3
+    return res
+
+
+def cubic_grad_kernel(xI, xJ, h):
+    # Derivative of cubic spline smoothing kernel
+    r = np.array(xI) - np.array(xJ)
+    r_len = np.linalg.norm(r)
+    r_dir = r / r_len if r_len != 0 else r
+
+    half_h = h / 2
+    k = __CUBIC_KERNEL_FACTOR / half_h**2
+    q = r_len / half_h
+
+    res = np.zeros_like(r, dtype=float)
+    if q < 1.0:
+        res = (k / half_h) * (-3. * q + 2.25 * q**2) * r_dir
+    elif q < 2.0:
+        res = -0.75 * (k / half_h) * (2. - q)**2 * r_dir
+    return res
+
+
 def w(xI, xJ, h):
-    kernel_cubic_spline(xI, xJ, h)
+    return cubic_kernel(xI, xJ, h)
 
 
 def grad_w(xI, xJ, h):
-    grad_kernel_cubic_spline(xI, xJ, h)
+    return cubic_grad_kernel(xI, xJ, h)
