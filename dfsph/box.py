85277750aac77eee754a3f061e7f4ffec13ea194
davidAlgis <david.algis@tutamail.com>
2025-03-10 18:14:27 +0100

    ✨ Add a box class

---
+++ dfsph/box.py
@@ -0,0 +1,26 @@
+from numba import njit
+from numba.experimental import jitclass
+from numba import int32, float32
+
+# Define the Box class specification
+box_spec = [
+    ('origin_x', float32),
+    ('origin_y', float32),
+    ('size_x', float32),
+    ('size_y', float32),
+]
+
+
+# Define the Box class
+@jitclass(box_spec)
+class Box:
+
+    def __init__(self, origin_x, origin_y, size_x, size_y):
+        self.origin_x = origin_x
+        self.origin_y = origin_y
+        self.size_x = size_x
+        self.size_y = size_y
+
+    def is_inside(self, x, y):
+        return (self.origin_x <= x <= self.origin_x + self.size_x
+                and self.origin_y <= y <= self.origin_y + self.size_y)
